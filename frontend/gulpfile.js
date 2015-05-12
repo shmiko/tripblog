e9a2ec55a892482fe203b299c63be7fe34c6293d
Paul Jones <shmiko@gmail.com>
2015-05-13 07:57:07 +1000

    backend/frontend

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,21 +1,27 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var mainBowerFiles = require('main-bower-files');
 
 var paths = {
     temp: 'temp',
     index: 'app/index.html'
-}
+};
 
-gulp.task('default',['tempSetup', 'serve']);
+gulp.task('default',['tempSetup','scripts', 'serve']);
+
+gulp.task('scripts', function(){
+    gulp.src(mainBowerFiles())
+    .pipe(gulp.dest(paths.temp));
+});
 
 gulp.task('tempSetup', function(){
     gulp.src(paths.index)
-    .pipe(gulp.dest('temp'));
-})
+    .pipe(gulp.dest(paths.temp));
+});
 
 gulp.task('serve', function(){
     gulp.src(paths.temp)
     .pipe(webserver({
         open: true
     }));
-})
+});
