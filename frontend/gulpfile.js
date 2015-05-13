b1bc9add056bb08c57a2b1cb8993714bf8c26b2f
Paul Jones <shmiko@gmail.com>
2015-05-13 19:47:14 +1000

    main file??

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+//var mainBowerFiles = require('main-bower-files');
 var mainBowerFiles = require('main-bower-files');
-
 var paths = {
     temp: 'temp',
     index: 'app/index.html'
 };
 
-gulp.task('default',['tempSetup','scripts', 'serve']);
+gulp.task('default',['tempSetup','scriptjs', 'serve']);
+
+gulp.task('scriptjs', function() {
+  return gulp.src(mainBowerFiles({checkExistence: true}, { base: 'bower_components' }))
+    .pipe(gulp.dest(paths.temp))
+});
 
 gulp.task('scripts', function(){
-    gulp.src(mainBowerFiles())
+    gulp.src(mainBowerFiles(
+      //{
+      //  paths: {
+      //      bowerDirectory: 'bower_components',
+      //      //bowerrc: '.bowerrc',
+      //      bowerJson: 'bower.json'
+      //  }
+      //}
+    ))
     .pipe(gulp.dest(paths.temp));
 });
 
